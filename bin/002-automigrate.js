ac20f9fbbc470290942956b07e54c62ccbd7bfa4
Arthur Tkachenko <atherdon@gmail.com>
2018-02-18 08:47:19 +0200

    wokrs

---
+++ bin/002-automigrate.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var path     = require('path');
+
+let app      = require(path.resolve(__dirname, '../server/server'));
+var database = app.datasources.searchDS;
+
+
+var lbTables = [
+  'Attribute',
+  'Department',
+	'Ingredient',
+	'Menu',
+  'Recipe'
+];
+
+//creating loopback necessary tables if no exists
+database.automigrate(lbTables, function(err) {
+  if (err) throw err;
+
+  console.log( 'Loopback tables [' + lbTables.toString() + '] created in ' + database.adapter.name );
+  database.disconnect();
+
+  // process.on('exit', function(code) {
+  // 	return console.log(`Automigrate is competed`);
+  // });
+  // process.exit(22);
+});
--- server/models/attribute.js
+++ server/models/attribute.js
@@ -1,7 +1,10 @@
-
-
 // it's the place were we'll store all stuff.
 
 // And i want to have a remote methods and things, that i can easily call 'diets' and get stuff related only for ingredients
 
 // and then i'll came to a simple method where i only include or exclude ids
+'use strict';
+
+module.exports = function(Attribute) {
+
+};
