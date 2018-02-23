25529934c891a69211caf86c2407c9028cceebd6
Arthur Tkachenko <atherdon@gmail.com>
2018-02-23 08:52:03 +0200

    *****

--- server/boot/root.js
+++ server/boot/root.js
@@ -4,5 +4,46 @@ module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
+
+  router.get('/ziza', function(req, res, next){
+
+		// var ingredientId = req.params.id;
+		// var groceryId = req.params.groceryId;
+		
+		var Ing   = app.models.Ingredient;
+		Ing.find({
+			where: 
+				{
+					or: [
+						"name": {
+							"like": "oil"
+						},
+						"name": {
+							"like": "apple"
+						}
+					]
+				}
+			}
+			// {
+			// 		and: [
+			// 			"name": {
+			// 				"like": "oil"
+			// 			},
+			// 			"name": {
+			// 				"like": "apple"
+			// 			}
+			// 		]
+			// 	}
+			// }
+		});
+		
+
+
+		// .findById(departmentId, {}, function(err, model){
+		//   model.updateAttribute('visible', false);
+		// });
+	});
+
+
   server.use(router);
 };
--- server/models/ingredient.js
+++ server/models/ingredient.js
@@ -1,5 +1,5 @@
 'use strict';
 
-module.exports = function(Ingredients) {
+module.exports = function(Ingredient) {
 
 };
